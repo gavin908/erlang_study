f1c89578c8e02e25e7f9095fc0c8d32c1cc96e06
ebihebi <bin.h.he@ericsson.com>
2017-06-07 17:45:24 +0800

    aa

---
+++ chapter14/src/kitty_gen_server.erl
@@ -0,0 +1,37 @@
+%%%-------------------------------------------------------------------
+%%% @author ebihebi
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 07. Jun 2017 14:15
+%%%-------------------------------------------------------------------
+-module(kitty_gen_server).
+-compile(export_all).
+-behavior(gen_server).
+
+start_link() ->
+  gen_server:start_link(?MODULE, [a, b], []).
+
+order_cat(Pid, Name, Color, Description) ->
+  gen_server:call(Pid, {order, Name, Color, Description}).
+
+init(Args) ->
+  io:format("[~p]Server init is called, Args:~p~n", [self(), Args]),
+  {ok, []}.
+
+handle_call(Request, From, State) ->
+  io:format("[~p]Server handle a call, Request:~p, State:~p~n", [self(), Request, State]),
+  {reply, ["myResult"], State}.
+
+handle_cast(Request, State) ->
+  erlang:error(not_implemented).
+
+handle_info(Info, State) ->
+  erlang:error(not_implemented).
+
+terminate(Reason, State) ->
+  erlang:error(not_implemented).
+
+code_change(OldVsn, State, Extra) ->
+  erlang:error(not_implemented).
